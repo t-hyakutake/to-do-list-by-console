4ce6f291ee2fc82bc2a3cd936d6074454bd026fc
githubt-hyakutake <tomohiro_h_1979@yahoo.co.jp>
2023-01-14 03:48:33 +0900

    exit,キャンセルでtodo終了

---
+++ mainjs.js
@@ -0,0 +1,23 @@
+'use strict'
+let cmd = prompt('new,list,delete,exit のいずれかを入力ください')
+while (cmd !== 'exit' && cmd !== null){
+  
+  
+  
+  switch (cmd) {
+    case 'new':
+      console.log('new')
+      console.log('**********')
+      break;
+    case 'list':
+        console.log('list')
+        console.log('**********')
+      break;
+    case 'delete':
+          console.log('delete')
+          console.log('**********')
+  }
+        
+        cmd = prompt('new,list,delete,exit のいずれかを入力ください')
+}
+        console.log('todolistを終了します')
