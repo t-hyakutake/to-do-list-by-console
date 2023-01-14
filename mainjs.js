1d44b361f033756dfab90e28c44f3695e848727b
githubt-hyakutake <tomohiro_h_1979@yahoo.co.jp>
2023-01-14 16:19:33 +0900

    ifに変更も変わらず

--- mainjs.js
+++ mainjs.js
@@ -1,32 +1,31 @@
 'use strict'
 let cmd = prompt('new,list,delete,exit のいずれかを入力ください')
-while (cmd !== 'exit' && cmd !== null) {
-  let todo = ['ライブ','握手会']
- 
-  
-  
-  switch (cmd) {
-    case 'new':
-      let addTodo = prompt('to do リストを追加してください。')
-      todo.push(addTodo);
-      console.log(`${addTodo}を追加しました`)
-      // break;
-    case 'list':
-        for (let i = 0; i < todo.length; i++) {
-          console.log(`${i + '：' + todo[i]}`)
-        }
-      console.log(todo);
-      console.log('**********')
-      // break;
-    case 'delete':
-      let del = prompt('削除index番号を入力してください');
-      let delIndex = todo.splice(del,1);
-      console.log(todo)
+let todo = ['ライブ','握手会']
 
-      console.log(`${delIndex}を削除します`)
-      console.log('**********')
-  }
+while (cmd !== 'exit' && cmd !== null) {
   cmd = prompt('new,list,delete,exit のいずれかを入力ください')
+
+  if (cmd === 'list') {
+    for (let i = 0; i < todo.length; i++) {
+      console.log(`${i + '：' + todo[i]}`)
+    }
+  console.log('**********')
+  } else if (cmd === 'new') {
+    let addTodo = prompt('to do リストを追加してください。')
+        todo = todo.push(addTodo);
+        console.log(`${addTodo}を追加しました`)
+        console.log(todo)
+    
+  } else if (cmd === 'delete') {
+    let del = prompt('削除index番号を入力してください');
+       let delIndex = todo.splice(del,1);
+         console.log(todo)
+  
+         console.log(`${delIndex}を削除します`)
+         console.log('**********')
+  } 
+  
+
 }
 console.log('todolistを終了します');
 console.log(todo);
